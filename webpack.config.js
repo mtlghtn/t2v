a09271325c73e8ef0185a699cea872d56f70faaf
hawschiat <haw1997@hotmail.com>
2018-09-15 15:01:05 -0400

    vuejs components set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -8,18 +9,25 @@ module.exports = {
     path: path.resolve(__dirname, 'public')
   },
   module: {
-        rules: [{
-			test: /\.scss$/,
-			use: ExtractTextPlugin.extract(
-			  {
-				fallback: 'style-loader',
-				use: ['css-loader', 'sass-loader']
-			  })
-		}]
+        rules: [
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract(
+				  {
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				  })
+			},
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			}
+		]
     },
 	plugins: [ 
     new ExtractTextPlugin(
       {filename: 'style.css'}
-    )
+    ),
+	new VueLoaderPlugin()
   ]
 };
