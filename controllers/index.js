f69d8365b3f7dcd832a350748a25b5a207ab8386
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-09-15 17:56:13 -0400

    fixed index redirect route to allow passing data to the ejs via render()

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,15 @@
-var express = require('express')
-  , router = express.Router();
+var express = require('express'),
+    prepositions = require('./prepositions'),
+    position = require('./position'),
+    router = express.Router();
 
 router.post('/', function(req, res) {
-    res.redirect('/result');
+    res.render('result', {});
 });
 
+/*
 router.get('/result', function(req, res) {
     res.render('result', {})
 });
-
+*/
 module.exports = router
