e3381081b390275f24b8f48af9891e5cccc25bc1
hawschiat <haw1997@hotmail.com>
2018-09-15 16:49:30 -0400

    xss prevention script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './scss/index.scss'
+
+(function($){
+    $(function(){
+        //Run on DOM ready
+        const body = $('body');
+
+        //validate form before submit, also strip tags to prevent XSS
+        body.on('submit', '#scriptInput', function(e){
+            e.preventDefault();
+            var scriptInput = $('#scriptInput'),
+                textContent = scriptInput.find('.input'),
+                submitText = textContent.val();
+            textContent.val(stripHTML(submitText));
+
+            this.submit();
+        });
+
+        function stripHTML(dirtyString) {
+            var container = document.createElement('div');
+            var text = document.createTextNode(dirtyString);
+            container.appendChild(text);
+            return container.innerHTML; // innerHTML will be a xss safe string
+        }
+    })
+
+    //Anonymous functions...
+
+})(jQuery)
