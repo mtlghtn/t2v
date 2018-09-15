706caad1ea74b6be6454bc9cd222421bd9bf5f0e
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2018-09-15 16:18:58 -0400

    position.js logic, result.ejs route

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,11 @@ var express = require('express')
   , router = express.Router();
 
 router.post('/', function(req, res) {
-    res.render('result');
+    res.redirect('/result');
+});
+
+router.get('/result', function(req, res) {
+    res.render('result', {})
 });
 
 module.exports = router
---
+++ controllers/position.js
@@ -0,0 +1,70 @@
+/* structure of input:
+ *
+ * wordList = [
+ *  {
+ *      action: 'up',
+ *      imgPath: '../public/images',
+ *      imgX: 100,
+ *      imgY: 200,
+ *  }
+ * ]
+ *
+ * canvas = {
+ *  x: 800,
+ *  y: 600
+ * }
+ */
+
+const calculatePositions = function (wordList, canvas) {
+    var wordA = {
+        img: {
+            path: wordList[0].imgPath,
+            x: wordList[0].imgX,
+            y: wordList[0].imgY
+        },
+        action: wordList[0].action,
+        x: 0,
+        y: 0
+    };
+    var wordB = {
+        img: {
+            path: wordList[1].imgPath,
+            x: wordList[1].imgX,
+            y: wordList[1].imgY
+        },
+        action: wordList[1].action,
+        x: 0,
+        y: 0
+    };
+
+    if(wordA.action == 'center') {
+        // A in B
+        wordA.x = canvas.x/2 - wordA.img.x/2;
+        wordB.x = canvas.x/2 - wordB.img.x/2;
+        wordA.y = canvas.y - wordA.img.y;
+        wordB.y = canvas.y - wordB.img.y;
+    } else if (wordA.action == 'left') {
+        // A to the left, B to the right
+        wordA.x = canvas.x/4 - wordA.img.x/2;
+        wordB.x = canvas.x - canvas.x/4 - wordB.img.x/2;
+        wordA.y = canvas.y - wordA.img.y;
+        wordB.y - canvas.y - wordB.img.y;
+    } else if (wordA.action == 'up') {
+        // A over B
+        wordA.x = canvas.x/2 - wordA.img.x/2;
+        wordB.x = canvas.x/2 - wordB.img.x/2;
+        wordB.y = canvas.y - wordB.img.y;
+        wordA.y = canvas.y - wordB.img.y - wordA.img.y;
+    } else if (wordA.action == 'down') {
+        // A under B
+        wordA.x = canvas.x/2 - wordA.img.x/2;
+        wordB.x = canvas.x/2 - wordB.img.x/2;
+        wordA.y = canvas.y - wordA.img.y;
+        wordB.y = canvas.y - wordA.img.y - wordB.img.y;
+    }
+    return {wordA: wordA, wordB: wordB};
+}
+
+module.exports = {
+    calculatePositions: calculatePositions
+};
