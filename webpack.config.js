2bfd136adf0998464116015d1fea03eec336617c
hawschiat <haw1997@hotmail.com>
2018-09-15 14:01:10 -0400

    fix directory path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'public')
   },
   module: {
         rules: [{
